a06514514e7cf782139b86acbfcda49cce75c6dc
Arthur Zhuravetskiy <arthurzhuravetskiy@gmail.com>
2021-12-07 00:56:48 +0100

    Fixed more

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react'
 import BackgroundVideo from './components/backgroundVid/backgroundVid';
 import {BrowserRouter as Router,Route,
   Switch} from 'react-router-dom';
-import Navigbar from "./components/Navbar/Navigbar";
+
 import Explore from './components//explore/Explore';
 import Contact from './components//contact/contact';
-import Footer from './components/Footer/Footer';
+
 // import './allcss.css';
 
 
